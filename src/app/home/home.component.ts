fc115093f6142d728f67a6d31ae4645fddb95d04
Jose Hervás <jhervasdiaz@gmail.com>
2019-11-17 13:45:00 +0100

    sugerencia de mejora

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,7 +26,46 @@ export class HomeComponent {
     )
 
     this.busqueda = "";
-    this._data.llamadaGet("https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?language=es&from=2019-11-15&q=noticia&apiKey=afea889eae7d463b855fd2b78dc6c4c7&pageSize=12");
+
+    /******
+     SUGERENCIA DE MEJORA:
+
+     Ahora mismo, cada vez que alguien entra en la url /home
+     se hace la llamada GET a la API.
+
+     PERO, si esa persona ha entrado primero en el path /articulo/:id
+     entonces la llamada GET ya se habrá hecho (ver commit anterior a este).
+
+     Por lo tanto, el service YA tendrá la info guardada en _data.articles
+     y esta llamada no será necesaria.
+
+     CONCLUSIÓN: Al cargarse el componente, comprobamos si el data service
+     ya tiene la info cargada. Si no la tiene, hacemos la llamada GET. 
+     Si la tiene, cogemos los datos del service y nos ahorramos esa llamada.
+
+    *******/
+    if (Object.entries(this._data.articles).length === 0){
+      this._data.llamadaGet("https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?language=es&from=2019-11-15&q=noticia&apiKey=afea889eae7d463b855fd2b78dc6c4c7&pageSize=12");
+    } else {
+      this.data = this._data.articles;
+      this.arrArticulos = this.data["articles"];
+    }
+
+    /*
+                   *
+             / \
+            /___\
+           ( o o )            * *
+           )  L  (           /   * *
+   ________()(-)()________  /     * * *
+ E\| _____ )()()() ______ |/B     * * *
+   |/      ()()()(       \|      * * * *
+           | )() |
+           /     \
+          / *  *  \
+         /   *  *  \
+        / *_  *  _  \   PUFFF 
+     */
 
 
 
